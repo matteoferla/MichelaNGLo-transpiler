1bc481514da0f1e6b53f409d1106ba02ea8be0b9
matteoferla <matteo.ferla@gmail.com>
2022-03-09 15:10:24 +0000

    :beetle: setup error

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ import setuptools
 ##from setuptools.command.install import install
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+
+import os
+this_directory = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(this_directory,'requirements.txt'), 'r') as fh:
+        requirements = fh.read().strip().split()
+    with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = fh.read()
+except Exception:  # weird file
+    requirements = ['biopython', 'pymol']
+    long_description = ''
 
 setuptools.setup(
     name="michelanglo_transpiler",
-    version="1.0.0",
+    version="1.0.1",
     author="Matteo Ferla",
     author_email="matteo@well.ox.ac.uk",
     description="Transpiler module for Michelanglo",
@@ -18,7 +29,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],install_requires=open('requirements.txt')
+    ],install_requires=requirements
 )
 
 '''
